8a4a58526b39dece83b20d79b6d25f902e4a0f93
MateusRodr <mr5508113@gmail.com>
2025-02-27 13:28:41 -0300

    conf url

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './estilizacao/index.css';
 import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
+import * as React from 'react';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
